808aa61edcd6d639d9a76c1d5cea1b9d01c6b29e
Kushal Singh Thakur <singhkushal937@gmail.com>
2026-04-19 13:36:39 +0530

    Add: shortest-path-in-binary-matrix - submission-2

---
+++ Data Structures & Algorithms/shortest-path-in-binary-matrix/submission-2.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int shortestPathBinaryMatrix(int[][] grid) {
+
+        if(grid == null || grid.length == 0 || grid[0].length == 0 ) {
+            return -1;
+        }
+
+        int length = 0, rows = grid.length, cols = grid[0].length;
+
+        Deque<int[]> que = new ArrayDeque<>();
+
+        que.addLast(new int[]{0, 0});
+        boolean[][] visited = new boolean[rows][cols];
+
+        while(!que.isEmpty()) {
+            int size = que.size();
+
+            for(int i = 0; i < size; i++) {
+                int[] pair = que.pollFirst();
+                int r = pair[0];
+                int c = pair[1];
+
+                if(r < 0 || c < 0 || r >= rows || c >= cols || grid[r][c] == 1) {
+                    continue;
+                }
+
+                if(visited[r][c]) {
+                    continue;
+                }
+
+                visited[r][c] = true;
+
+                if(grid[r][c] == 0 && r == rows-1 && c == cols - 1) {
+                    return length+1;
+                }
+
+                que.addLast(new int[]{r-1, c-1});
+                que.addLast(new int[]{r-1, c});
+                que.addLast(new int[]{r-1, c+1});
+                que.addLast(new int[]{r, c-1});
+                que.addLast(new int[]{r, c+1});
+                que.addLast(new int[]{r+1, c-1});
+                que.addLast(new int[]{r+1, c});
+                que.addLast(new int[]{r+1, c+1});
+            }
+
+            length++;
+        }
+
+
+        return -1;
+    }
+}
