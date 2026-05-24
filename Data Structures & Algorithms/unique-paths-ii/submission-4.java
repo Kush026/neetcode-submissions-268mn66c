aa01391c369228cc3379b116aac7c54d30636f85
Kushal Singh <singhkushal937@gmail.com>
2026-05-24 19:29:15 +0530

    Add: unique-paths-ii - submission-4

---
+++ Data Structures & Algorithms/unique-paths-ii/submission-4.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int rows = obstacleGrid.length;
+        int cols = obstacleGrid[0].length;
+
+        int[] dp = new int[cols];
+        dp[0] = 1;
+
+        for(int i = 0; i < rows; i++) {
+
+            if(obstacleGrid[i][0] == 1) {
+                dp[0] = 0;
+            }
+
+            for(int j = 1; j < cols; j++) {
+
+                if(obstacleGrid[i][j] == 1) {
+                    dp[j] = 0;
+                }
+                else {
+                    dp[j] += dp[j-1];
+                }
+            }
+        }
+
+
+        return dp[cols-1];
+    }
+}
